6917ec6bf554bed152a85f397fd258826e7a3736
Arturo Arroyo <arturoarroyo916@gmail.com>
2020-06-29 21:55:02 -0500

    Responde mejor ante los inicios de sesión, para que no regrese a la pagina de inicio si ya estas logeado

--- contenedor_front_end/progra_segura/prograSegura/views.py
+++ contenedor_front_end/progra_segura/prograSegura/views.py
@@ -16,17 +16,16 @@ def login(request):
 	t = 'login_v2.html' 
 	if request.method == 'GET' and not request.session.get('logueado', False):
 		return render(request, t)
-	elif request.method == 'GET' and request.session.get('logueado', False):
-		return redirect('/monitor')
-	elif request.method == 'POST' and request.session.get('logueado', False):
-		return redirect('/monitor')
 	elif request.method == 'POST' and not request.session.get('logueado', False):
 		if back_end.dejar_pasar_peticion_login(request):
 			usuario = request.POST.get('usuario','')
 			contra = request.POST.get('password','')
-			user = authenticate(username=usuario,password=contra)
-			if user is not None:
+			user2 = authenticate(username=usuario,password=contra)
+			if user2 is not None:
+				if request.user.is_active == True:
+					return render(request,t,{'errores': 'Este usuario ya tiene una sesión activa'})
 				request.session['logueado'] = True
+				request.session['usuario'] = usuario
 				#respuesta = redirect('/verificar_usuario')
 				return redirect('/verificar_usuario')
 				#return HttpResponseRedirect(reverse_lazy('verificar_usuario'))
@@ -34,6 +33,15 @@ def login(request):
 				return render(request, t,{'errores': 'Usuario o contraseña incorrectos'})
 		else:
 			return render(request,t,{'errores': 'Demasiados intentos fallidos'})
+	elif request.method == 'GET' and request.session.get('logueado', False):
+		return redirect('/monitor')
+	elif request.method == 'POST' and request.session.get('logueado', False):
+		return redirect('/monitor')
+
+@esta_logueado
+def generar_nuevo_token(request):
+	request.session['token_enviado'] = False
+	return redirect('/verificar_usuario')
 
 @esta_logueado
 def verificar_usuario(request):
@@ -42,18 +50,21 @@ def verificar_usuario(request):
 	#request.session['token']=token_b64
 	if request.method == 'GET' and not request.session.get('verificado', False) :
 		print("HOLA")
-		#token_b64=back_end.generar_token()
+		if  request.session.get('token_enviado', False):
+			return render(request,t,{'errores': 'Token enviado'})
 		request.session['token']=back_end.generar_token()
 		request.session['timestamp']=datetime.timestamp(datetime.now())
 		print("HOLA2")
 		#test = back_end.telegram_bot_sendtext(token_b64)
 		#prueba=request.session.get('token')
 		#token=request.session.get('token')
+
 		back_end.telegram_bot_sendtext(request.session.get('token'))
-		print("Ya paso")
+		request.session['token_enviado'] = True
 		return render(request,t)
 	elif request.method == 'GET' and request.session.get('verificado', True):
 		return redirect('/monitor')
+
 	elif request.method == 'POST':
 		#if request.session.get('verificado', True):
 			#return redirect('/monitor')
@@ -65,7 +76,7 @@ def verificar_usuario(request):
 				request.session['verificado'] = True
 				return redirect('/monitor')
 			else:
-				if request.session.get('verificado', True):
+				if request.session.get('verificado', False):
 					return redirect('/monitor')
 				else:
 					return redirect('/logout')
